cff60b2d499fe0d54243e7d1a38dceb9dbe8812f
Casta626 <jacastaneda@iesrafaelalberti.es>
2022-02-10 18:54:07 +0100

    d

---
+++ main.py
@@ -0,0 +1,274 @@
+import sys
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QWizard, QWizardPage,QLineEdit, QHBoxLayout, QLabel, QComboBox, QTextEdit, QVBoxLayout,QMessageBox, QAbstractItemView
+from reportlab.pdfgen.canvas import Canvas
+from pdfrw import PdfReader
+from pdfrw.buildxobj import pagexobj
+from pdfrw.toreportlab import makerl
+# Para ajustar el texto de los comentarios
+import textwrap
+# Para poner la fecha de hoy
+from datetime import datetime
+from PySide6.QtSql import QSqlDatabase, QSqlQuery, QSqlRelation, QSqlRelationalTableModel
+from PySide6.QtCore import Qt
+from pathlib import Path
+from PySide6.QtCore import QUrl, QRect
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QWizard, QWizardPage, QLineEdit, QHBoxLayout, QLabel
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtWebEngineCore import QWebEngineSettings
+from PySide6.QtWidgets import QApplication, QMainWindow
+import pyqtgraph as pg
+import sys
+
+from pyqtgraph.Qt import QtGui
+
+from ui_design2 import Ui_MainWindow
+class MainWindow(QMainWindow,Ui_MainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        
+
+        self.graphWidget = pg.PlotWidget(self.tabDatos)
+        self.graphWidget.setGeometry(QRect(10, 360, 441, 251))
+        self.verticalLayout_2.addWidget(self.graphWidget)
+
+        # Para mostrar un PDF, es necesario habilitar los plugins. Los plugins están en https://doc.qt.io/qtforpython/PySide6/QtWebEngineCore/QWebEngineSettings.html#detailed-description
+        self.webpdf.settings().setAttribute(QWebEngineSettings.PluginsEnabled, True)
+
+        # Con Path guardamos la ruta relativa al documento
+        rutaConPDF2 = Path("result.pdf")
+        
+        # Cargamos el fichero con la ruta absoluta como uri
+        # Usando http o https también se pueden cargar páginas web
+        self.webpdf.load(QUrl(rutaConPDF2.absolute().as_uri()))
+#####################################################################
+        self.pushButton.pressed.connect(self.generateGraphic)
+        self.pushButton_2.pressed.connect(self.generatePDF)
+
+        
+# Esta era para añadir la gráfica al pdf
+    def generateGraphic(self):
+        plt = pg.plot([1,5,2,4,3])
+
+        # Creamos una instancia de exportación con el ítem que queremos exportar
+        exporter = pg.exporters.ImageExporter(plt.plotItem)
+
+        # Establecemos los parámetros de la exportación (anchura)
+        exporter.parameters()['width'] = 100   # (afecta a la altura de forma proporcional)
+
+        # Elegimos el nombre del archivo en el que exportamos la gráfica como imagen
+        exporter.export('graphic.png')
+
+        outfile = "result.pdf"
+
+        template = PdfReader("template.pdf", decompress=False).pages[0]
+        template_obj = pagexobj(template)
+        canvas = Canvas(outfile)
+
+        xobj_name = makerl(canvas, template_obj)
+        canvas.doForm(xobj_name)
+
+        ystart = 650
+        canvas.drawImage("graphic.png", 50, ystart, width=None,height=None,mask=None)
+
+        template_obj = pagexobj(template)
+
+        # Creamos un canvas de reportlab para dibujar en el PDF resultante
+        canvas = Canvas(outfile)
+
+        # Generamos un objeto canvas con la plantilla
+        xobj_name = makerl(canvas, template_obj)
+        # Y lo añadimos al canvas creado para el resultado
+        canvas.doForm(xobj_name)
+
+        # Elegimos la altura de la imagen
+        ystart = 250
+
+        # Dibujamos la imagen dejando 50 píxeles a la izquierda
+        canvas.drawImage("graphic.png", 50, ystart, width=None,height=None,mask=None)
+
+        # Guardamos el canvas
+        canvas.save()
+        
+
+# esta era para hacer la grafica abajo
+    def generateGraphi(self):
+
+        self.data = {
+            'x1': str(self.spinBox.value()),
+            'x2': str(self.spinBox_2.value()),
+            'x3': str(self.spinBox_3.value()),
+            'x4': str(self.spinBox_4.value()),
+            'x5': str(self.spinBox_5.value()),
+            'x6': str(self.spinBox_6.value()),
+            'x7': str(self.spinBox_7.value()),
+            'x8': str(self.spinBox_8.value()),
+            'x9': str(self.spinBox_9.value()),
+            'x10': str(self.spinBox_10.value()),
+            'y1': str(self.spinBox_11.value()),
+            'y2': str(self.spinBox_12.value()),
+            'y3': str(self.spinBox_13.value()),
+            'y4': str(self.spinBox_14.value()),
+            'y5': str(self.spinBox_15.value()),
+            'y6': str(self.spinBox_16.value()),
+            'y7': str(self.spinBox_17.value()),
+            'y8': str(self.spinBox_18.value()),
+            'y9': str(self.spinBox_19.value()),
+            'y10': str(self.spinBox_20.value())
+            
+        }
+
+         # Todas las gráficas son un widget PlotWidget
+        # Proporciona un canvas en el que dibujar cualquier gráfica
+        self.graphWidget = pg.PlotWidget(self.tabDatos)
+        self.graphWidget.setGeometry(QRect(10, 360, 441, 251))
+        # self.setCentralWidget(self.graphWidget)
+
+        # Representamos los puntos de hora y temperatura
+        x = [str(self.spinBox.value()),str(self.spinBox_2.value()),str(self.spinBox_3.value()),str(self.spinBox_4.value()),str(self.spinBox_5.value()),
+        str(self.spinBox_6.value()),str(self.spinBox_7.value()),str(self.spinBox_8.value()),str(self.spinBox_9.value()),str(self.spinBox_10.value())]
+        y = [str(self.spinBox_11.value()),str(self.spinBox_12.value()),str(self.spinBox_13.value()),str(self.spinBox_14.value()),str(self.spinBox_15.value()),
+        str(self.spinBox_16.value()),str(self.spinBox_17.value()),str(self.spinBox_18.value()),str(self.spinBox_19.value()),str(self.spinBox_20.value())]
+
+        # Podemos cambiar el fondo, con muchas formas de definir el color
+        # self.graphWidget.setBackground('w')
+        # self.graphWidget.setBackground('#bbccaa')
+        # self.graphWidget.setBackground((100,50,255,25)) #RGBA
+        self.graphWidget.setBackground(QtGui.QColor(100,50,254,25))
+
+        # Y los dibujamos (x,y)
+        self.graphWidget.plot(x, y)
+        
+
+    def generatePDF(self):
+
+        # Para mostrar un PDF, es necesario habilitar los plugins. Los plugins están en https://doc.qt.io/qtforpython/PySide6/QtWebEngineCore/QWebEngineSettings.html#detailed-description
+        self.webpdf.settings().setAttribute(QWebEngineSettings.PluginsEnabled, True)
+
+        # Con Path guardamos la ruta relativa al documento
+        rutaConPDF2 = Path("result.pdf")
+        
+        # Cargamos el fichero con la ruta absoluta como uri
+        # Usando http o https también se pueden cargar páginas web
+        self.webpdf.load(QUrl(rutaConPDF2.absolute().as_uri()))
+        # self.web.load(QUrl("https://github.com/"))
+
+        self.data = {
+            'x1': str(self.spinBox.value()),
+            'x2': str(self.spinBox_2.value()),
+            'x3': str(self.spinBox_3.value()),
+            'x4': str(self.spinBox_4.value()),
+            'x5': str(self.spinBox_5.value()),
+            'x6': str(self.spinBox_6.value()),
+            'x7': str(self.spinBox_7.value()),
+            'x8': str(self.spinBox_8.value()),
+            'x9': str(self.spinBox_9.value()),
+            'x10': str(self.spinBox_10.value()),
+            'y1': str(self.spinBox_11.value()),
+            'y2': str(self.spinBox_12.value()),
+            'y3': str(self.spinBox_13.value()),
+            'y4': str(self.spinBox_14.value()),
+            'y5': str(self.spinBox_15.value()),
+            'y6': str(self.spinBox_16.value()),
+            'y7': str(self.spinBox_17.value()),
+            'y8': str(self.spinBox_18.value()),
+            'y9': str(self.spinBox_19.value()),
+            'y10': str(self.spinBox_20.value())
+            
+        }
+        outfile = "result.pdf"
+
+        template = PdfReader("template.pdf", decompress=False).pages[0]
+        template_obj = pagexobj(template)
+
+        canvas = Canvas(outfile)
+
+        xobj_name = makerl(canvas, template_obj)
+        canvas.doForm(xobj_name)
+
+        ystart = 600
+
+        canvas.drawString(150, ystart, self.data['x1'])
+        canvas.drawString(150, ystart-(23*1), self.data['x2'])
+        canvas.drawString(150, ystart-(24*2), self.data['x3'])
+        canvas.drawString(150, ystart-(23*3), self.data['x4'])
+        canvas.drawString(150, ystart-(24*4), self.data['x5'])
+        canvas.drawString(150, ystart-(23*5), self.data['x6'])
+        canvas.drawString(150, ystart-(24*6), self.data['x7'])
+        canvas.drawString(150, ystart-(23*7), self.data['x8'])
+        canvas.drawString(150, ystart-(24*8), self.data['x9'])
+        canvas.drawString(150, ystart-(23*9), self.data['x10'])
+
+        canvas.drawString(430, ystart, self.data['y1'])
+        canvas.drawString(430, ystart-(23*1), self.data['y2'])
+        canvas.drawString(430, ystart-(24*2), self.data['y3'])
+        canvas.drawString(430, ystart-(23*3), self.data['y4'])
+        canvas.drawString(430, ystart-(24*4), self.data['y5'])
+        canvas.drawString(430, ystart-(23*5), self.data['y6'])
+        canvas.drawString(430, ystart-(24*6), self.data['y7'])
+        canvas.drawString(430, ystart-(23*7), self.data['y8'])
+        canvas.drawString(430, ystart-(24*8), self.data['y9'])
+        canvas.drawString(430, ystart-(23*9), self.data['y10'])
+
+
+        # # Ponemos la fecha de hoy
+        # # today = datetime.today()
+        # # canvas.drawString(480, ystart, today.strftime('%F'))
+
+        # # Lo ideal es partir de una posición y jugar con el tamaño de la fuente
+        # # En este caso, cada línea son 28 puntos
+        # canvas.drawString(294, ystart, self.data['x2'])
+        # # canvas.drawString(230, ystart-28, self.data['apellidos'])
+
+        # canvas.drawString(175, ystart-(32), self.data['x3'])
+
+        # canvas.drawString(290, ystart-(32), self.data['x4'])
+
+        # canvas.drawString(423, ystart-(32), self.data['x5'])
+
+        # canvas.drawString(168, ystart-(2*32), self.data['x6'])
+
+        # canvas.drawString(285, ystart-(2*32), self.data['x7'])
+
+        # canvas.drawString(128, ystart-(3*32), self.data['x8'])
+
+        # canvas.drawString(472, ystart-(3*32), self.data['x9'])
+
+        # # canvas.drawString(472, ystart-(2*32), self.data['prueba'])
+
+        # # Sería posible establecer un límite en el número de caracteres:
+        # # field.setMaxLength(25)
+
+        # # Reemplazamos los saltos de línea por espacios en los comentarios
+        # comments = self.data['x10'].replace('\n', ' ')
+        # if comments:
+        #     # Separamos el texto de la primera línea (más corta que el resto)
+        #     lines = textwrap.wrap(comments, width=65)
+        #     # Nos quedamos la primera línea
+        #     first_line = lines[0]
+        #     # Guardamos el resto en remainder
+        #     remainder = ' '.join(lines[1:])
+
+        #     # Separamos el resto con una anchura mayot
+        #     lines = textwrap.wrap(remainder, 75)
+        #     # Nos quedamos con las cuatro primeras que son el máximo (sin incluir la primera)
+        #     lines = lines[:4]
+
+        #     # Escribimos la primera línea
+        #     canvas.drawString(147, ystart-(4*32), first_line)
+        #     # Y luego las otras cuatro
+        #     for n, l in enumerate(lines, 1):
+        #         canvas.drawString(80, ystart-(4*32) - (n*32), l)
+
+        canvas.save()
+        QMessageBox.information(self, "Finalizado", "Se ha generado el PDF")
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.setWindowTitle('Informes automatizados')
+window.show()
+app.exec()
---
+++ ui_design.py
@@ -0,0 +1,375 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'design.ui'
+##
+## Created by: Qt User Interface Compiler version 6.2.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QListView,
+    QMainWindow, QMenuBar, QPushButton, QSizePolicy,
+    QSpacerItem, QSpinBox, QStatusBar, QTabWidget,
+    QVBoxLayout, QWidget)
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtWebEngineCore import QWebEngineSettings
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(488, 688)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tabWidget.setGeometry(QRect(0, 0, 481, 651))
+        self.tabDatos = QWidget()
+        self.tabDatos.setObjectName(u"tabDatos")
+        self.verticalLayoutWidget = QWidget(self.tabDatos)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(120, 10, 198, 342))
+        self.verticalLayout_2 = QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.label = QLabel(self.verticalLayoutWidget)
+        self.label.setObjectName(u"label")
+
+        self.verticalLayout.addWidget(self.label)
+
+        self.label_2 = QLabel(self.verticalLayoutWidget)
+        self.label_2.setObjectName(u"label_2")
+
+        self.verticalLayout.addWidget(self.label_2)
+
+        self.label_3 = QLabel(self.verticalLayoutWidget)
+        self.label_3.setObjectName(u"label_3")
+
+        self.verticalLayout.addWidget(self.label_3)
+
+        self.label_4 = QLabel(self.verticalLayoutWidget)
+        self.label_4.setObjectName(u"label_4")
+
+        self.verticalLayout.addWidget(self.label_4)
+
+        self.label_5 = QLabel(self.verticalLayoutWidget)
+        self.label_5.setObjectName(u"label_5")
+
+        self.verticalLayout.addWidget(self.label_5)
+
+        self.label_6 = QLabel(self.verticalLayoutWidget)
+        self.label_6.setObjectName(u"label_6")
+
+        self.verticalLayout.addWidget(self.label_6)
+
+        self.label_7 = QLabel(self.verticalLayoutWidget)
+        self.label_7.setObjectName(u"label_7")
+
+        self.verticalLayout.addWidget(self.label_7)
+
+        self.label_8 = QLabel(self.verticalLayoutWidget)
+        self.label_8.setObjectName(u"label_8")
+
+        self.verticalLayout.addWidget(self.label_8)
+
+        self.label_9 = QLabel(self.verticalLayoutWidget)
+        self.label_9.setObjectName(u"label_9")
+
+        self.verticalLayout.addWidget(self.label_9)
+
+        self.label_10 = QLabel(self.verticalLayoutWidget)
+        self.label_10.setObjectName(u"label_10")
+
+        self.verticalLayout.addWidget(self.label_10)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout)
+
+        self.verticalLayout_3 = QVBoxLayout()
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.spinBox = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox.setObjectName(u"spinBox")
+
+        self.verticalLayout_3.addWidget(self.spinBox)
+
+        self.spinBox_2 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_2.setObjectName(u"spinBox_2")
+
+        self.verticalLayout_3.addWidget(self.spinBox_2)
+
+        self.spinBox_3 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_3.setObjectName(u"spinBox_3")
+
+        self.verticalLayout_3.addWidget(self.spinBox_3)
+
+        self.spinBox_4 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_4.setObjectName(u"spinBox_4")
+
+        self.verticalLayout_3.addWidget(self.spinBox_4)
+
+        self.spinBox_5 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_5.setObjectName(u"spinBox_5")
+
+        self.verticalLayout_3.addWidget(self.spinBox_5)
+
+        self.spinBox_6 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_6.setObjectName(u"spinBox_6")
+
+        self.verticalLayout_3.addWidget(self.spinBox_6)
+
+        self.spinBox_7 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_7.setObjectName(u"spinBox_7")
+
+        self.verticalLayout_3.addWidget(self.spinBox_7)
+
+        self.spinBox_8 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_8.setObjectName(u"spinBox_8")
+
+        self.verticalLayout_3.addWidget(self.spinBox_8)
+
+        self.spinBox_9 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_9.setObjectName(u"spinBox_9")
+
+        self.verticalLayout_3.addWidget(self.spinBox_9)
+
+        self.spinBox_10 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_10.setObjectName(u"spinBox_10")
+
+        self.verticalLayout_3.addWidget(self.spinBox_10)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout_3)
+
+
+        self.horizontalLayout_3.addLayout(self.horizontalLayout)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_3.addItem(self.horizontalSpacer)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.verticalLayout_4 = QVBoxLayout()
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.label_11 = QLabel(self.verticalLayoutWidget)
+        self.label_11.setObjectName(u"label_11")
+
+        self.verticalLayout_4.addWidget(self.label_11)
+
+        self.label_12 = QLabel(self.verticalLayoutWidget)
+        self.label_12.setObjectName(u"label_12")
+
+        self.verticalLayout_4.addWidget(self.label_12)
+
+        self.label_13 = QLabel(self.verticalLayoutWidget)
+        self.label_13.setObjectName(u"label_13")
+
+        self.verticalLayout_4.addWidget(self.label_13)
+
+        self.label_14 = QLabel(self.verticalLayoutWidget)
+        self.label_14.setObjectName(u"label_14")
+
+        self.verticalLayout_4.addWidget(self.label_14)
+
+        self.label_15 = QLabel(self.verticalLayoutWidget)
+        self.label_15.setObjectName(u"label_15")
+
+        self.verticalLayout_4.addWidget(self.label_15)
+
+        self.label_16 = QLabel(self.verticalLayoutWidget)
+        self.label_16.setObjectName(u"label_16")
+
+        self.verticalLayout_4.addWidget(self.label_16)
+
+        self.label_17 = QLabel(self.verticalLayoutWidget)
+        self.label_17.setObjectName(u"label_17")
+
+        self.verticalLayout_4.addWidget(self.label_17)
+
+        self.label_18 = QLabel(self.verticalLayoutWidget)
+        self.label_18.setObjectName(u"label_18")
+
+        self.verticalLayout_4.addWidget(self.label_18)
+
+        self.label_19 = QLabel(self.verticalLayoutWidget)
+        self.label_19.setObjectName(u"label_19")
+
+        self.verticalLayout_4.addWidget(self.label_19)
+
+        self.label_20 = QLabel(self.verticalLayoutWidget)
+        self.label_20.setObjectName(u"label_20")
+
+        self.verticalLayout_4.addWidget(self.label_20)
+
+
+        self.horizontalLayout_2.addLayout(self.verticalLayout_4)
+
+        self.verticalLayout_5 = QVBoxLayout()
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.spinBox_11 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_11.setObjectName(u"spinBox_11")
+
+        self.verticalLayout_5.addWidget(self.spinBox_11)
+
+        self.spinBox_12 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_12.setObjectName(u"spinBox_12")
+
+        self.verticalLayout_5.addWidget(self.spinBox_12)
+
+        self.spinBox_13 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_13.setObjectName(u"spinBox_13")
+
+        self.verticalLayout_5.addWidget(self.spinBox_13)
+
+        self.spinBox_14 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_14.setObjectName(u"spinBox_14")
+
+        self.verticalLayout_5.addWidget(self.spinBox_14)
+
+        self.spinBox_15 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_15.setObjectName(u"spinBox_15")
+
+        self.verticalLayout_5.addWidget(self.spinBox_15)
+
+        self.spinBox_16 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_16.setObjectName(u"spinBox_16")
+
+        self.verticalLayout_5.addWidget(self.spinBox_16)
+
+        self.spinBox_17 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_17.setObjectName(u"spinBox_17")
+
+        self.verticalLayout_5.addWidget(self.spinBox_17)
+
+        self.spinBox_18 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_18.setObjectName(u"spinBox_18")
+
+        self.verticalLayout_5.addWidget(self.spinBox_18)
+
+        self.spinBox_19 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_19.setObjectName(u"spinBox_19")
+
+        self.verticalLayout_5.addWidget(self.spinBox_19)
+
+        self.spinBox_20 = QSpinBox(self.verticalLayoutWidget)
+        self.spinBox_20.setObjectName(u"spinBox_20")
+
+        self.verticalLayout_5.addWidget(self.spinBox_20)
+
+
+        self.horizontalLayout_2.addLayout(self.verticalLayout_5)
+
+
+        self.horizontalLayout_3.addLayout(self.horizontalLayout_2)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_3)
+
+        self.pushButton = QPushButton(self.verticalLayoutWidget)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.verticalLayout_2.addWidget(self.pushButton)
+
+        self.pushButton_2 = QPushButton(self.verticalLayoutWidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.verticalLayout_2.addWidget(self.pushButton_2)
+
+        self.web = QWebEngineView(self.tabDatos)
+        self.web.setObjectName(u"QWebEngineView")
+        self.web.setGeometry(QRect(10, 360, 441, 251))
+
+        
+
+        # self.listView = QListView(self.tabDatos)
+        # self.listView.setObjectName(u"listView")
+        # self.listView.setGeometry(QRect(10, 360, 441, 251))
+
+        self.tabWidget.addTab(self.tabDatos, "")
+        self.tabInforme = QWidget()
+        self.tabInforme.setObjectName(u"tabInforme")
+        self.tabWidget.addTab(self.tabInforme, "")
+        self.webPDF = QWebEngineView(self.tabInforme)
+        self.webPDF.setObjectName(u"QWebEngineView2")
+        self.webPDF.setGeometry(QRect(0, 0, 441, 251))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 488, 22))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+#if QT_CONFIG(shortcut)
+        self.label.setBuddy(self.spinBox)
+        self.label_2.setBuddy(self.spinBox_2)
+        self.label_3.setBuddy(self.spinBox_3)
+        self.label_4.setBuddy(self.spinBox_4)
+        self.label_5.setBuddy(self.spinBox_5)
+        self.label_6.setBuddy(self.spinBox_6)
+        self.label_7.setBuddy(self.spinBox_7)
+        self.label_8.setBuddy(self.spinBox_8)
+        self.label_9.setBuddy(self.spinBox_9)
+        self.label_10.setBuddy(self.spinBox_10)
+        self.label_11.setBuddy(self.spinBox)
+        self.label_12.setBuddy(self.spinBox)
+        self.label_13.setBuddy(self.spinBox)
+        self.label_14.setBuddy(self.spinBox)
+        self.label_15.setBuddy(self.spinBox)
+        self.label_16.setBuddy(self.spinBox)
+        self.label_17.setBuddy(self.spinBox)
+        self.label_18.setBuddy(self.spinBox)
+        self.label_19.setBuddy(self.spinBox)
+        self.label_20.setBuddy(self.spinBox)
+#endif // QT_CONFIG(shortcut)
+
+        self.retranslateUi(MainWindow)
+
+        self.tabWidget.setCurrentIndex(0)
+
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Dato x1", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Dato x2", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Dato x3", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Dato x4", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Dato x5", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"Dato x6", None))
+        self.label_7.setText(QCoreApplication.translate("MainWindow", u"Dato x7", None))
+        self.label_8.setText(QCoreApplication.translate("MainWindow", u"Dato x8", None))
+        self.label_9.setText(QCoreApplication.translate("MainWindow", u"Dato x9", None))
+        self.label_10.setText(QCoreApplication.translate("MainWindow", u"Dato x10", None))
+        self.label_11.setText(QCoreApplication.translate("MainWindow", u"Dato y1", None))
+        self.label_12.setText(QCoreApplication.translate("MainWindow", u"Dato y2", None))
+        self.label_13.setText(QCoreApplication.translate("MainWindow", u"Dato y3", None))
+        self.label_14.setText(QCoreApplication.translate("MainWindow", u"Dato y4", None))
+        self.label_15.setText(QCoreApplication.translate("MainWindow", u"Dato y5", None))
+        self.label_16.setText(QCoreApplication.translate("MainWindow", u"Dato y6", None))
+        self.label_17.setText(QCoreApplication.translate("MainWindow", u"Dato y7", None))
+        self.label_18.setText(QCoreApplication.translate("MainWindow", u"Dato y8", None))
+        self.label_19.setText(QCoreApplication.translate("MainWindow", u"Dato y9", None))
+        self.label_20.setText(QCoreApplication.translate("MainWindow", u"Dato y10", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Generar Gr\u00e1fica", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Generar Informe", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tabDatos), QCoreApplication.translate("MainWindow", u"Datos", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tabInforme), QCoreApplication.translate("MainWindow", u"Informe", None))
+    # retranslateUi
+
